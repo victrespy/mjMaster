db4507d9cd0006c20903e3644c31d529be2bac4e
memnochdavid <davduquediaz@gmail.com>
2026-02-16 18:33:28 +0100

    complete skeleton - fixtures, scripts

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -7,7 +7,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
