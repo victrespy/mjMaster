fc05055b5af4d4e95a54375e7498397e0a0cd85a
memnochdavid <davduquediaz@gmail.com>
2026-02-23 09:39:49 +0100

    README.md y GUIA_TRABAJO_EQUIPO.md con solución para error de login

--- backup_db_full.sql
+++ backup_db_full.sql
@@ -2,10 +2,10 @@
 -- PostgreSQL database dump
 --
 
-\restrict wavUlo3AY2pX7JUh8bsh1oV6cmbSbLd5iAz7AThj2KyXcGVr4R3jtHSP9KywJlM
+\restrict veDm9foTwBtn5vIaA7uqgRC7XH8byGYfvR1ITyyTgdhsUQIM68yc94aJ9b3xRId
 
--- Dumped from database version 15.16
--- Dumped by pg_dump version 15.16
+-- Dumped from database version 15.15
+-- Dumped by pg_dump version 15.15
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -274,6 +274,7 @@ COPY public.categories (id, name, created_at, deleted_at, deleted) FROM stdin;
 --
 
 COPY public.doctrine_migration_versions (version, executed_at, execution_time) FROM stdin;
+DoctrineMigrations\\Version20260219072540	2026-02-19 07:25:54	2509
 DoctrineMigrations\\Version20260218180326	2026-02-18 18:03:27	60
 \.
 
@@ -291,8 +292,7 @@ COPY public.messenger_messages (id, body, headers, queue_name, created_at, avail
 --
 
 COPY public.order_products (id, quantity, unit_price, created_at, deleted_at, deleted, order_id, product_id) FROM stdin;
-5	1	45.90	2026-02-18 18:13:11	\N	f	3	15
-6	1	12.00	2026-02-18 18:13:11	\N	f	3	16
+11	6	35.00	2026-02-23 07:33:00	\N	f	5	14
 \.
 
 
@@ -301,7 +301,7 @@ COPY public.order_products (id, quantity, unit_price, created_at, deleted_at, de
 --
 
 COPY public.orders (id, total, state, created_at, deleted_at, deleted, user_id) FROM stdin;
-3	57.90	COMPLETED	2026-02-18 18:13:11	\N	f	8
+5	210.00	COMPLETED	2026-02-23 07:33:00	\N	f	9
 \.
 
 
@@ -311,10 +311,10 @@ COPY public.orders (id, total, state, created_at, deleted_at, deleted, user_id) 
 
 COPY public.products (id, name, description, price, stock, picture, created_at, deleted_at, deleted, category_id) FROM stdin;
 13	Amnesia Haze (3 uds)	Semillas feminizadas de alta calidad con sabor cítrico.	25.50	100	/products/placeholder.avif	2026-02-18 18:13:11	\N	f	9
-14	Northern Lights Auto (5 uds)	Variedad autofloreciente clásica, ideal para principiantes.	35.00	50	/products/placeholder.avif	2026-02-18 18:13:11	\N	f	9
 15	Bong de Vidrio 30cm	Bong de borosilicato resistente con percolador.	45.90	15	/products/placeholder.avif	2026-02-18 18:13:11	\N	f	10
 16	Grinder Metálico 4 partes	Grinder de aluminio con polinizador y espátula.	12.00	200	/products/placeholder.avif	2026-02-18 18:13:11	\N	f	10
 17	Kit Fertilizantes Orgánicos	Pack completo para crecimiento y floración.	29.95	30	/products/placeholder.avif	2026-02-18 18:13:11	\N	f	11
+14	Northern Lights Auto (5 uds)	Variedad autofloreciente clásica, ideal para principiantes.	35.00	44	/products/placeholder.avif	2026-02-18 18:13:11	\N	f	9
 \.
 
 
@@ -334,6 +334,7 @@ COPY public.reviews (id, rating, comment, created_at, deleted_at, deleted, user_
 COPY public.users (id, name, email, password, address, phone, roles, created_at, deleted_at, deleted) FROM stdin;
 7	Grow Master Admin	admin@example.com	$2y$13$OT4vYxCCiPC/m60dJrW8tu0qw32bJQLEAzjSc2DTOqEKY40Tn975G	Calle del Humo 420, Madrid	\N	["ROLE_ADMIN"]	2026-02-18 18:13:10	\N	f
 8	Juan Cultivador	user@example.com	$2y$13$nY.c6cT58FJqEb5Xl6ivkOF8ZbLd13SOx9so7Y9Ot2Gir4cJSJnaG	Avenida de la Resina 7, Barcelona	\N	["ROLE_USER"]	2026-02-18 18:13:10	\N	f
+9	David Duque Díaz	david@mj.com	$2y$13$Qyga7.KcWfho.xNkj4PcI.T8wcrTFWm7XODZm6KSJ8GSyMGQDyHJ2	\N	\N	[]	2026-02-20 11:11:09	\N	f
 \.
 
 
@@ -355,14 +356,14 @@ SELECT pg_catalog.setval('public.messenger_messages_id_seq', 1, false);
 -- Name: order_products_id_seq; Type: SEQUENCE SET; Schema: public; Owner: app_user
 --
 
-SELECT pg_catalog.setval('public.order_products_id_seq', 6, true);
+SELECT pg_catalog.setval('public.order_products_id_seq', 11, true);
 
 
 --
 -- Name: orders_id_seq; Type: SEQUENCE SET; Schema: public; Owner: app_user
 --
 
-SELECT pg_catalog.setval('public.orders_id_seq', 3, true);
+SELECT pg_catalog.setval('public.orders_id_seq', 5, true);
 
 
 --
@@ -383,7 +384,7 @@ SELECT pg_catalog.setval('public.reviews_id_seq', 3, true);
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: app_user
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 8, true);
+SELECT pg_catalog.setval('public.users_id_seq', 11, true);
 
 
 --
@@ -572,5 +573,5 @@ ALTER TABLE ONLY public.orders
 -- PostgreSQL database dump complete
 --
 
-\unrestrict wavUlo3AY2pX7JUh8bsh1oV6cmbSbLd5iAz7AThj2KyXcGVr4R3jtHSP9KywJlM
+\unrestrict veDm9foTwBtn5vIaA7uqgRC7XH8byGYfvR1ITyyTgdhsUQIM68yc94aJ9b3xRId
 
